0dc035feb664ced999fefac022e413c03a8c9707
Michel <leyou.m@gmail.com>
2015-02-03 16:35:08 +0100

    fixes

--- app/Wysiwyg/Bundle.php
+++ app/Wysiwyg/Bundle.php
@@ -2,7 +2,7 @@
 namespace Wysiwyg;
 
 class Bundle extends \Asgard\Core\BundleLoader {
-	public function run($container) {
-		$container['widgetsManager']->setWidget('wysiwyg', 'Wysiwyg\WysiwygWidget');
+	public function run(\Asgard\Container\ContainerInterface $container) {
+		$container['widgetManager']->setWidget('wysiwyg', 'Wysiwyg\WysiwygWidget');
 	}
 }
